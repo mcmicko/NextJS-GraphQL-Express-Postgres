2de144093f0e16ee3234c5534caa40a7f472b46e
mcmicko <mcmzrc@gmail.com>
2021-01-28 20:31:46 +0100

    Login & Registration

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,14 +1,15 @@
 import { MikroORM } from "@mikro-orm/core";
+import path from "path";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
-import path from "path";
+import { User } from "./entities/User";
 
 export default {
   migrations: {
     path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "ben-awad-react-graph",
   user: "postgres",
   password: "zrenjaninac",
