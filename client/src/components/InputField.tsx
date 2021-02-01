2f32bb99fef173a16b4fc107233a3a03c98f4346
mcmicko <mcmzrc@gmail.com>
2021-02-01 20:04:01 +0100

    Set Up Frontend & Create Registar Page

---
+++ client/src/components/InputField.tsx
@@ -0,0 +1,31 @@
+import React, { InputHTMLAttributes } from "react";
+import { Field, useField } from "formik";
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  placeholder: string;
+  label: string;
+};
+
+// !! -> '' => false, 'error message stuff => true
+
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _,
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htnkFor={field.name}>{label}</FormLabel>
+      <Input {...props} {...field} id={Field.name} />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
---
+++ client/src/pages/register.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Form, Formik } from "formik";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { Box, Button } from "@chakra-ui/react";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              colorScheme="teal"
+              type="submit"
+            >
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
