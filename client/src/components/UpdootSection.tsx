5fbd3815c6b14409ad4672a210ee4931ac27c28d
mcmicko <mcmzrc@gmail.com>
2021-04-06 19:22:13 +0200

    Upvote UI

---
+++ client/src/components/UpdootSection.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { ArrowDownIcon, ArrowUpIcon } from "@chakra-ui/icons";
+import { Flex } from "@chakra-ui/react";
+import { PostSnippetFragment, useVoteMutation } from "../generated/graphql";
+
+interface UpdootSectionProps {
+  post: PostSnippetFragment;
+}
+
+export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
+  const [loadingState, setLoadingState] = useState<
+    "updoot-loading" | "downdoot-loading" | "not-loading"
+  >("not-loading");
+  const [, vote] = useVoteMutation();
+  return (
+    <Flex direction="column" justifyContent="center" alignItems="center" mr={4}>
+      <ArrowUpIcon
+        onClick={async () => {
+          setLoadingState("updoot-loading");
+          await vote({ postId: post.id, value: 1 });
+          setLoadingState("not-loading");
+        }}
+        isLoading={loadingState === "updoot-loading"}
+      />
+      {post.points}
+      <ArrowDownIcon
+        onClick={async () => {
+          setLoadingState("downdoot-loading");
+          await vote({ postId: post.id, value: -1 });
+          setLoadingState("not-loading");
+        }}
+        isLoading={loadingState === "downdoot-loading"}
+      />
+    </Flex>
+  );
+};
